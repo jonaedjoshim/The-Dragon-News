70c87a719cf92d727ef97570d923019694e324db
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-18 00:11:19 +0600

    ...

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,18 @@
 import React, { use } from "react";
+import { NavLink } from "react-router";
 const categoryPromise = fetch("/categories.json").then((res) => res.json());
 
 const Categories = () => {
-    console.log(categoryPromise);
-    const categories = use(categoryPromise)    
+  console.log(categoryPromise);
+  const categories = use(categoryPromise);
   return (
-    <div className="font-bold">
-      <h2>All categories ({categories.length}) </h2>
+    <div>
+      <h2 className="font-semibold text-xl">All categories ({categories.length}) </h2>
+      <div className="grid grid-cols-1 mt-5 gap-6">
+        {categories.map((category) => (
+          <NavLink key={category.id} to={`/category/${category.id}`} className={"btn bg-white border-none shadow-none hover:bg-base-200 font-medium text-xl text-accent"}>{category.name}</NavLink>
+        ))}
+      </div>
     </div>
   );
 };
