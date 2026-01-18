9e4e3bd7ea7dd630ed9c92eef69693af80d9f44d
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-19 01:01:40 +0600

    added find us section

--- src/components/HomeLayout/RightAside.jsx
+++ src/components/HomeLayout/RightAside.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import SocialLogin from './SocialLogin';
+import React from "react";
+import SocialLogin from "./SocialLogin";
+import FindUs from "./FindUs";
 
 const RightAside = () => {
   return (
-    <div>
-       <SocialLogin/>
+    <div className="space-y-8">
+      <SocialLogin />
+      <FindUs />
     </div>
   );
 };
